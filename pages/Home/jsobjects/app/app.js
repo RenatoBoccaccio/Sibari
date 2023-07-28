92a0ccd39e2ad6562b5c62c00c9abae61de45fcb
Renato Boccaccio <renato.boccaccio@libero.it>
2023-07-28 13:47:46 +0000

    System generated commit, initial commit

---
+++ pages/Home/jsobjects/app/app.js
@@ -0,0 +1,40 @@
+export default {
+	createHash:(password) => {
+		return dcodeIO.bcrypt.hashSync(password , 10);
+	},
+	verifyHash: (password , hash) => {
+		return dcodeIO.bcrypt.compareSync(password , hash);
+	},
+	createToken: (email) => {
+		return jsonwebtoken.sign({email}, 'secret', {expiresIn: 60*60});
+	},
+	
+	iscriviti: () => {
+		const nome = name_su.text;
+		const password = password_su.text;
+		const tipo = tipo_su.text;
+
+		const pHash = this.createHash(password);
+		return signup.run({nome, pHash, tipo})
+			.then(() => showAlert('Account creato !', 'success'))
+			.then(() =>'token', storeValue(this.createToken(name)))
+			.then(() => navigateTo('MainPage'))
+			.catch(e => showAlert(e.message, 'errore'));
+	},
+	signIn: async () => {
+		const nome = name_si.text;
+		const password = password_si.text;
+		
+		const [user] = await finduser.run({nome});
+		
+		if(user && this.verifyHash(password, user?.password)){
+			if(user?.tipo == 'Admin'){
+				navigateTo('MainPage')
+			} else {
+				navigateTo('ReadPage')
+			}
+		} else {
+			showAlert('Nome o password non corretti', 'error');
+		}
+	}
+}
